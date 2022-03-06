f119393994db1791836bc876a6adb88460972852
XcodeBoi <runningzoomcat@gmail.com>
2022-03-06 11:35:51 +1100

    feat: Compoisition exports; documation

--- __init__.py
+++ __init__.py
@@ -15,15 +15,22 @@ def new(parent: Union[tk.Frame, tk.Tk], formLayout: Any) -> FormFrame:
 from .widgets.raw import generic, multi
 
 class raw: 
+    Widget = generic.Widget
     TextField = generic.TextField
     EntryField = generic.EntryField
     DropDown = generic.DropDown
     RadioButtons = multi.RadioButtons
     MultiDrop = multi.MultiDrop
 
-# export error classes for external use
+# accessor for composition related things
+from .widgets.raw.multiwidgetBase import MultiWidget
 
+class composition:
+    Multi = MultiWidget
+
+# export error classes for external use
 from . import errors
 
 class error:
     UiError = errors.UiError
+
