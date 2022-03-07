17fcdafada434e44573e73b8be8d6591d582a9a6
XcodeBoi <runningzoomcat@gmail.com>
2022-03-07 20:58:07 +1100

    unsafe support

--- __init__.py
+++ __init__.py
@@ -34,3 +34,20 @@ from . import errors
 class error:
     UiError = errors.UiError
 
+# unsafe things. Avert your gaze and wish you wern't here
+# exports for things intended to be private, but we allow because I'm lazy and implemented some
+# stuff in fairly ok ways i want to use in other projects but don't want to make sure the api
+# works for every use case
+from .widgets.namedWidget import NamedWidget
+
+class unsafe:
+    class composition:
+        # NamedWidget is supposed to be private, but a nice use case for it occured
+        # in an external project so I allow it to be exported
+        # this is hacky. hacky is bad except where the project is small scale
+        # and interwoven unexpected deps don't make your head hurt too much
+        NamedWidget = NamedWidget
+        
+
+# TODO: style interface for mutating the base Widget class.
+# Also document that it must be called prior to widget construction
